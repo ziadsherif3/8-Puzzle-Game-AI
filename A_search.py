ee82a6582a4d6b6595db6772a4a8e60360a41a29
ziadsherif3 <ziadsherif3@gmail.com>
2021-02-10 02:06:50 +0200

    Done

--- A_search.py
+++ A_search.py
@@ -1,5 +1,6 @@
 import heapq
 import math
+import time
 
 MANHATTAN = 1
 EUCLIDEAN = 2
@@ -81,17 +82,112 @@ class Board():
         return self.confg == other.confg
     
     def __str__(self):
-        return f'{self.confg}'
+        return f'{self.confg}\n'
 
 goal_state = Board([[0, 1, 2], [3, 4, 5], [6, 7, 8]], None, None, None)
 
-def search(board):
-    initial_state_manh = Board(board, MANHATTAN, None, 0)
-    initial_state_eucl = Board(board, EUCLIDEAN, None, 0)
+def decrease_key(heap, board):
+    indx = heap.index(board)
+    old_board = heap[indx]
     
-    manh_out = A_star_search(initial_state_manh)
-    eucl_out = A_star_search(initial_state_eucl)
-    
+    if board.total_cost < old_board.total_cost:
+        heap[indx] = board
+        heapq.heapify(heap)
+
 def A_star_search(initial_state: Board):
-    pass
+    frontier = [initial_state]
+    explored = []
+    
+    while len(frontier) != 0:
+        state = heapq.heappop(frontier)
+        explored.append(state)
+        
+        if state == goal_state:
+            return [True, state, explored]
+        
+        for neighbor in state.get_neighbors():
+            if (neighbor not in frontier) and (neighbor not in explored):
+                heapq.heappush(frontier, neighbor)
+            elif neighbor in frontier:
+                decrease_key(frontier, neighbor)
+        
+    return [False]
+
+def search(board):
+    print("Choose the heuristic function:\n1) Manhattan Distance\n2) Euclidean Distance\n")
+    choice = input().split(" ")
+    
+    while (len(choice) != 1) or (not choice[0].isdigit()) or (int(choice[0]) < 1) or (int(choice[0]) > 2):
+        print("Invalid input! Please retry.")
+        choice = input().split(" ")
+    
+    if choice[0] == '1':
+        initial_state_manh = Board(board, MANHATTAN, None, 0)
+        
+        manh_begin = time.time()
+        manh_out = A_star_search(initial_state_manh)
+        time.sleep(1)
+        manh_end = time.time()
+        
+        manh_time = manh_end - manh_begin
+        
+        if not manh_out[0]:
+            print("Failure, can't solve the puzzle.\n")
+            return
+        
+        search_depth = 0
+        
+        for board in manh_out[2]:
+            if board.cost > search_depth:
+                search_depth = board.cost
+        
+        goal_path = [manh_out[1]]
+        current_board = manh_out[1]
+        
+        while current_board.parent is not None:
+            goal_path.insert(0, current_board.parent)
+            current_board = current_board.parent
+        
+        print(f'Running time = {manh_time}\n')
+        print(f'Search depth = {search_depth}\n')
+        print(f'Cost of path = {manh_out[1].cost}\n')
+        print("Nodes expanded:\n", *manh_out[2], "\n")
+        print("Path to goal:\n", *goal_path, "\n")
+        
+    elif choice[0] == '2':
+        initial_state_eucl = Board(board, EUCLIDEAN, None, 0)
+        
+        eucl_begin = time.time()
+        eucl_out = A_star_search(initial_state_eucl)
+        time.sleep(1)
+        eucl_end = time.time()
+        
+        eucl_time = eucl_end - eucl_begin
+        
+        if not eucl_out[0]:
+            print("Failure, can't solve the puzzle.\n")
+            return
+        
+        search_depth = 0
+        
+        for board in eucl_out[2]:
+            if board.cost > search_depth:
+                search_depth = board.cost
+        
+        goal_path = [eucl_out[1]]
+        current_board = eucl_out[1]
+        
+        while current_board.parent is not None:
+            goal_path.insert(0, current_board.parent)
+            current_board = current_board.parent
+            
+        print(f'Running time = {eucl_time}\n')
+        print(f'Search depth = {search_depth}\n')
+        print(f'Cost of path = {eucl_out[1].cost}\n')
+        print("Nodes expanded:\n", *eucl_out[2], "\n")
+        print("Path to goal:\n", *goal_path, "\n")
+        
+    
+    
+    
     
